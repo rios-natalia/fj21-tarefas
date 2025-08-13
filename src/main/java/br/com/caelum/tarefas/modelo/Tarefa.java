8f94346d2e0bc700eac260a3e087956e0714cd68
natalia <rios-natalia>
2025-08-13 16:30:10 -0300

    chore: add jpa and hibernate

--- src/main/java/br/com/caelum/tarefas/modelo/Tarefa.java
+++ src/main/java/br/com/caelum/tarefas/modelo/Tarefa.java
@@ -1,52 +1,56 @@
 package br.com.caelum.tarefas.modelo;
 
 import java.util.Calendar;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import org.springframework.format.annotation.DateTimeFormat;
 
+@Entity
 public class Tarefa {
-   private Long id;
-   @NotNull
-   @Size(
-      min = 5
-   )
-   private String descricao;
-   private boolean finalizado;
-   @DateTimeFormat(
-      pattern = "dd/MM/yyyy"
-   )
-   private Calendar dataFinalizacao;
+	@Id
+	@GeneratedValue
+	private Long id;
+	@NotNull
+	@Size(min = 5)
+	private String descricao;
+	private boolean finalizado;
+	@Temporal(TemporalType.DATE)
+	private Calendar dataFinalizacao;
 
-   public Long getId() {
-      return this.id;
-   }
+	public Long getId() {
+		return this.id;
+	}
 
-   public void setId(Long id) {
-      this.id = id;
-   }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-   public String getDescricao() {
-      return this.descricao;
-   }
+	public String getDescricao() {
+		return this.descricao;
+	}
 
-   public void setDescricao(String descricao) {
-      this.descricao = descricao;
-   }
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 
-   public boolean isFinalizado() {
-      return this.finalizado;
-   }
+	public boolean isFinalizado() {
+		return this.finalizado;
+	}
 
-   public void setFinalizado(boolean finalizado) {
-      this.finalizado = finalizado;
-   }
+	public void setFinalizado(boolean finalizado) {
+		this.finalizado = finalizado;
+	}
 
-   public Calendar getDataFinalizacao() {
-      return this.dataFinalizacao;
-   }
+	public Calendar getDataFinalizacao() {
+		return this.dataFinalizacao;
+	}
 
-   public void setDataFinalizacao(Calendar dataFinalizacao) {
-      this.dataFinalizacao = dataFinalizacao;
-   }
+	public void setDataFinalizacao(Calendar dataFinalizacao) {
+		this.dataFinalizacao = dataFinalizacao;
+	}
 }
